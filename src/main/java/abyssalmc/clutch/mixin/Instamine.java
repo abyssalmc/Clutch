f882ca3ea3e4a00b620c363e2f428607cd68f154
abyssalmc <lberg388@gmail.com>
2026-06-27 23:56:55 +1000

    instamining fix

--- src/main/java/abyssalmc/clutch/mixin/Instamine.java
+++ src/main/java/abyssalmc/clutch/mixin/Instamine.java
@@ -2,10 +2,18 @@ package abyssalmc.clutch.mixin;
 
 import abyssalmc.clutch.GlobalDataHandler;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.MiningToolItem;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,13 +29,20 @@ public abstract class Instamine {
             if (MinecraftClient.getInstance().isIntegratedServerRunning() && MinecraftClient.getInstance().getServer() != null) {
                 ItemStack held = self.getMainHandStack();
 
-                float speed = held.getMiningSpeedMultiplier(state);
-                float hardness = state.getHardness(self.getWorld(), null);
-                System.out.println(hardness);
+                float speed = held.getMiningSpeedMultiplier(state); // > 1 if right tool type
 
-                // instamine condition (ignoring falling)
-                if (speed >= hardness * 30.0f) {
-                    cir.setReturnValue(1000000.0f);
+                if (speed > 1f) {
+                    if (state.isOf(Blocks.DAYLIGHT_DETECTOR)
+                            || state.isOf(Blocks.MOSS_BLOCK)
+                            || state.isOf(Blocks.SNOW_BLOCK)
+                            || state.isOf(Blocks.SCULK)
+                            || state.isIn(BlockTags.LEAVES)
+                            || state.isOf(Blocks.BROWN_MUSHROOM_BLOCK)
+                            || state.isOf(Blocks.RED_MUSHROOM_BLOCK)
+                            || state.isOf(Blocks.MUSHROOM_STEM)) {
+
+                        cir.setReturnValue(1000000f);
+                    }
                 }
             }
         }
